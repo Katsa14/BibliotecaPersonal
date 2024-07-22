0d4dad51cfa0de0d8f5a5c1b9c9205e363fbf3fa
Juli Arvely <juliacapiz@outlook.com>
2024-07-22 12:36:34 -0600

    Actualización de Usuario.java:
    Métodos get y set para nombre, contraseña y fecha de nacimiento.

--- src/Usuario.java
+++ src/Usuario.java
@@ -5,4 +5,40 @@
     private String nombre_usuario;
     private String contraseña_usuario;
     private String fecha_nacimiento; 
+
+    public String getNombre_Usuario(){
+ 
+        return nombre_usuario;
+
+    }
+
+    public void setNombre_Usuario(String nombre_usuario){
+
+        this.nombre_usuario = nombre_usuario;
+    }
+
+    public  String getContraseña_Usuario(){
+
+        return contraseña_usuario;
+
+    }
+
+    public void setContraseña_Usuario(String contraseña_usuario){
+
+        this.contraseña_usuario = contraseña_usuario;
+
+    }
+
+    public String getFecha_Nacimiento(){
+
+        return fecha_nacimiento;
+
+    }
+
+    public void setFecha_Nacimiento(String fecha_nacimiento){
+
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+ 
  }
