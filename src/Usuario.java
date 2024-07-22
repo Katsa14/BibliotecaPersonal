8377e58c1f7e3a4d29f2ec732053d02456cb8df6
Juli Arvely <juliacapiz@outlook.com>
2024-07-22 11:55:28 -0600

    Se Añadió la Clase al Usuario.java
    Tiene 3 privates strings, nombre, contraseña y
    fecha de nacimiento del usuario...

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,8 @@
  package com.java.juli.biblioteca.mislibros;
- 
+
+ public class Usuario{
+
+    private String nombre_usuario;
+    private String contraseña_usuario;
+    private String fecha_nacimiento; 
+ }
